32978e4924c72b15a2fbdea82292bd0bd48acc5e
subaru-git <subaru12081985@yahoo.co.jp>
2019-05-04 15:05:55 +0900

    disable firebase

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 require('dotenv').config()
-const remo = require('./remo')
+// const remo = require('./remo')
 const sensor = require('./sensor')
 const cron = require('node-cron')
-var firebase = require("firebase-admin");
-var serviceAccount = require("./setting/firebase-config.json");
+// var firebase = require("firebase-admin");
+// var serviceAccount = require("./setting/firebase-config.json");
 
 cron.schedule('* * * * *', () => {
   sensor.pushSensor();
 });
 
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: process.env.DATABASEURL
-});
+// firebase.initializeApp({
+//   credential: firebase.credential.cert(serviceAccount),
+//   databaseURL: process.env.DATABASEURL
+// });
 
-var db = firebase.database();
-var ref = db.ref("/");
+// var db = firebase.database();
+// var ref = db.ref("/");
 
-ref.on("child_changed", function (changedSnapshot) {
-  const l = changedSnapshot.val();
-  console.log(`light ${l}`);
-  if (l === "true") {
-    remo.lightOn()
-  } else if (l === "false") {
-    remo.lightOff()
-  }
-  ref.update({
-    light: "wait"
-  })
-});
+// ref.on("child_changed", function (changedSnapshot) {
+//   const l = changedSnapshot.val();
+//   console.log(`light ${l}`);
+//   if (l === "true") {
+//     remo.lightOn()
+//   } else if (l === "false") {
+//     remo.lightOff()
+//   }
+//   ref.update({
+//     light: "wait"
+//   })
+// });
